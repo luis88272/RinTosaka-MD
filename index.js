1f15c9ad731505837a13062f9f0935d8d48d4d6f
luis88272 <104382207+luis88272@users.noreply.github.com>
2024-04-10 05:31:43 -0500

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ say('The Mystic\nBot', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']});
-say(`Bot creado por Bruno Sobrino`, {
+say(`Bot creado por Bruno Jared`, {
   font: 'console',
   align: 'center',
   gradient: ['red', 'magenta']});
