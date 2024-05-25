de3ecec0d4f02e39a47ebc7f27f24cefbce7d751
luis88272 <104382207+luis88272@users.noreply.github.com>
2024-05-25 04:21:40 -0500

    Update config.js

--- config.js
+++ config.js
@@ -13,7 +13,7 @@ global.confirmCode = ""
 global.owner = [
   ['593985655462', '👑 Shadow Monarch 👑', true],
   ['50499698072', '💫 Mystic - Collaborator 1 💫', true],
-  ['51940617554', '💫 Mystic - Collaborator 2 💫', true],
+  ['523344895457', '💫 Mystic - Collaborator 2 💫', true],
   ['51996089079', '💫 Mystic - Collaborator 3 💫', true],
   ['5218442286089', '💫 Mystic - Collaborator 4 💫', true],
   ['50246028932', '💫 Mystic - Collaborator 5 💫', true],
